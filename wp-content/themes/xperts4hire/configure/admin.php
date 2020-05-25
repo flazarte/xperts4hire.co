a1352bf1e9652f823025a8340d1c2169d74f0586
Ferdinand Lazarte <ferdinand.lazarte@tell.com>
2020-05-25 16:20:00 +0800

    Geocode maps location

--- wp-content/themes/xperts4hire/configure/admin.php
+++ wp-content/themes/xperts4hire/configure/admin.php
@@ -1086,3 +1086,45 @@ function ps_redirect_after_logout(){
          wp_redirect( home_url('login') );
          exit();
 }
+
+
+// function to geocode address, it will return false if unable to geocode address
+function geocode($address){
+
+	$address = urlencode($address);
+	
+	// google map geocode api url used api key account ferdinandlazarte10@gmail.com xperts4hire project
+	$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyDMiEoNc_TxfTfSov0xUy6typg_R8sref0";
+   
+	// get the json response
+	$resp_json = file_get_contents($url);
+	$resp = json_decode($resp_json, true);
+	if($resp['status']=='OK'){
+
+		$lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+		$longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+		$formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+		
+		if($lati && $longi && $formatted_address){
+	
+			$data_arr = array();			
+			
+			array_push(
+				$data_arr, 
+					$lati, 
+					$longi, 
+					$formatted_address
+				);
+			
+			return $data_arr;
+			
+		}else{
+			return false;
+		}
+		
+	}
+	else{
+		echo "<strong>ERROR: {$resp['status']}</strong>";
+		return false;
+	}
+}
